d031a1b25543a1c0ab344814bc1314e7e8dc1c82
abdulsalamamtech <abdulsalamamtech@gmail.com>
2025-01-12 11:54:09 +0100

    Modified api route file

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\URL;
 Route::get('/', function () {
     return [
         'message' => 'Welcome to the API!',
-        'documentation_url' => url() . '/docs/api',
+        'documentation_url' => url('/') . '/docs/api',
         'contact_email' => 'abdulsalamamtech@gmail.com',
         'terms_of_service_url' => URL::current().'terms-of-service',
         'privacy_policy_url' => URL::current().'privacy-policy',
